3757260a717568722f930ed04877574219013dc4
Leon White <kirill.e500ov@gmail.com>
2025-06-26 13:48:16 +0300

    Fix

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,25 +1,28 @@
 from http import HTTPStatus
 
-from backend.api import models
 from django.test import Client, TestCase
+
+from backend.api import models
 
 
 class TaskiTestCase(TestCase):
-    def setup(self):
+    def setUp(self):
         self.client = Client()
 
     def test_api_list_available(self):
         response = self.client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_create_uncompleted_tast(self):
+    def test_create_uncompleted_task(self):
         data = {
             "title": "Title",
             "description": "Descr"
         }
         response = self.client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
-        self.assertTrue(models.Task.objects.get(
-            title=data["title"],
-            description=data["description"]).exists()
-                        )
+        self.assertTrue(
+            models.Task.objects.get(
+                title=data["title"],
+                description=data["description"]
+            ).exists()
+        )
